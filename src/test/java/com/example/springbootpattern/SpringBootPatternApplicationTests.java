33e64c43213f733a7fbaaaf654c12fee15472995
강은택 사원 <etkang@widetns.com>
2023-01-25 13:28:54 +0900

    SpringBootPattern 등록

---
+++ src/test/java/com/example/springbootpattern/SpringBootPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
